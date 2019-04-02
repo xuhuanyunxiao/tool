4a91ad4a7849f7266c7feca211c00889e2040594
xuhuan <605828203@qq.com>
2019-04-02 16:50:44 +0800

    20190402

--- toolkits/tricks/tricks_sklearn_wordcloud.py
+++ toolkits/tricks/tricks_sklearn_wordcloud.py
@@ -1,24 +1,3 @@
-#%% -----------------     jieba  ----------------------
-
-# 基于TextRank算法进行关键词抽取
-from jieba import analyse
-# 引入TextRank关键词抽取接口
-textrank = analyse.textrank
-text = "线程是程序执行时的最小单位，它是进程的一个执行流，\
-    是CPU调度和分派的基本单位，一个进程可以由很多个线程组成，\
-    线程间共享进程的所有资源，每个线程有自己的堆栈和局部变量。\
-    线程由CPU独立调度执行，在多CPU环境下就允许多个线程同时运行。\
-    同样多线程也可以实现并发操作，每个请求分配一个线程来处理。"
- 
-keywords = textrank(text)
-
-# 基于TF-IDF算法进行关键词抽取
-from jieba import analyse
-# 引入TF-IDF关键词抽取接口
-tfidf = analyse.extract_tags
-keywords = tfidf(text)
-
-
 #%% -----------------     wordcloud  ----------------------
 
 from wordcloud import WordCloud,STOPWORDS
